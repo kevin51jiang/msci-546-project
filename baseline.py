4eebbd94015d8ed3c49a4ba391781aefbe13c2f2
owo <owo@whats.this>
2024-03-11 13:58:11 -0400

    Cleanup

--- baseline.py
+++ baseline.py
@@ -5,6 +5,7 @@ import importlib
 
 importlib.reload(utils)
 
+# To run this, temporarily modify utils.train_and_report to not do PCA
 res = utils.train_and_report("Logistic Regression", LogisticRegression(), {})
 
 print(res)
